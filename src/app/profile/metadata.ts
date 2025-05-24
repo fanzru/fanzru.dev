0743861f4ab127fd581e00393c77c5dff30f7999
fanzru <fattahilaaf080701@gmail.com>
2025-05-25 01:05:56 +0700

    fix: node version volta

---
+++ src/app/profile/metadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Profile | Ananda Affan Fattahila | Fanzru",
+  description:
+    "Full Stack & AI Engineer with experience in web development, distributed systems, and software engineering.",
+};
